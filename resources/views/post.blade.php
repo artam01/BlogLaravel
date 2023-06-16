57d1859a50d23942b95b75c7a74f433110b1f2fb
Arta <arta.mustafa@versoaltima.com>
2023-06-16 11:00:15 +0200

    Commit message

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/post.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Post View</title>
+</head>
+<body>
+    <div>
+        {!! $post !!}
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}',function($slug){
+    $path=__DIR__."/../resources/posts/{$slug}.html";
+    $post=file_get_contents($path);
+     return view('post',[
+       'post'=> $post
+    ]);
+})->where('post','[A-z_\-]+');
