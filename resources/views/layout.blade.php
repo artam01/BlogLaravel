b32bbe447747c46893f35ddbdd28fb54d0ee3f3a
Arta Mustafa <artamustafa01@gmail.com>
2023-06-29 14:17:50 +0200

    first commit - commiting all files

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Application</title>
+    <!-- CSS and other meta tags -->
+</head>
+
+<body>
+    <header>
+        <!-- Header content, e.g., logo, navigation menu -->
+    </header>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    <footer>
+        <!-- Footer content -->
+    </footer>
+
+    <!-- JavaScript and other scripts -->
+</body>
+
+</html>
