b20086a77d7ef45feae22fb06dbf15ab8776c453
Arta Mustafa <artamustafa01@gmail.com>
2023-06-30 10:16:44 +0200

    Update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,16 +2,10 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
-=======
-class Post
-{
-   
->>>>>>> develop
 }
